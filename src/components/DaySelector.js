2ce957109d6a174407c0c41953236ef05fde4fca
Max Huttunen <max.huttunen@gmail.com>
2016-07-31 22:22:17 +0300

    fix a few bugs

--- src/components/DaySelector.js
+++ src/components/DaySelector.js
@@ -7,12 +7,12 @@ import { setDayOffset } from '../store/actions/values';
 let DaySelector = ({ setDayOffset }) => {
   return (
     <div className="day-selector">
-      <button onClick={setDayOffset(-1)}>-</button>
-      <button onClick={setDayOffset(1)}>+</button>
+      <button onClick={() => setDayOffset(-1)}>-</button>
+      <button onClick={() => setDayOffset(1)}>+</button>
     </div>
   )
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({ setDayOffset }, dispatch)
 
-export default connect(mapDispatchToProps)(DaySelector)
+export default connect(null, mapDispatchToProps)(DaySelector)

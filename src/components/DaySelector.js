f8f82b017a0a10900686871275bd630412b8bcb3
Max Huttunen <max.huttunen@gmail.com>
2016-08-20 17:27:42 +0300

    buttons per day

--- src/components/DaySelector.js
+++ src/components/DaySelector.js
@@ -3,36 +3,22 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import moment from 'moment'
 
-import { IoArrowLeftC, IoArrowRightC } from 'react-icons/lib/io'
-
 import { setDayOffset } from '../store/actions/values';
 
-const getDayString = (dayOffset, format) => {
-  return moment().add(dayOffset, 'day').locale('fi').format(format).toUpperCase()
-}
-
-const canSetDay = (dayOffset, delta) => {
-  return dayOffset + delta >= 0 && dayOffset + delta <= 7
-}
+const getDayString = (dayOffset, format) =>
+  moment().add(dayOffset, 'day').locale('fi').format(format).toUpperCase()
 
 const DaySelector = ({ dayOffset, setDayOffset }) => {
   return (
     <div className="dayselector">
-      <div className="dayselector-controls">
-        <button
-          className={!canSetDay(dayOffset, -1) ? 'button-disabled': ''}
-          disabled={!canSetDay(dayOffset, -1)}
-          onClick={() => setDayOffset(dayOffset - 1)}>
-          <p>{getDayString(dayOffset - 1, 'dddd')}</p>
-        </button>
-        <h1 className="dayselector-header">{getDayString(dayOffset, 'dddd, D.M.')}</h1>
-        <button
-          className={!canSetDay(dayOffset, 1) ? 'button-disabled' : ''}
-          disabled={!canSetDay(dayOffset, 1)}
-          onClick={() => setDayOffset(dayOffset + 1)}>
-          <p>{getDayString(dayOffset + 1, 'dddd')}</p>
-        </button>
-      </div>
+      {_.times(6, i =>
+      <button
+        key={i}
+        className={i === dayOffset ? 'selected' : ''}
+        onClick={() => setDayOffset(i)}>
+        {getDayString(i, 'dddd, DD.MM.')}
+      </button>
+      )}
     </div>
   )
 }

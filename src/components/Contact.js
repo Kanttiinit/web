c7fa4c5ff5ae3d44fb460d8b377f5959efe29013
Max Huttunen <max.huttunen@gmail.com>
2016-08-21 20:11:37 +0300

    styles for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,13 @@ import 'isomorphic-fetch'
 import PageContainer from './PageContainer'
 
 export default class Contact extends React.Component {
+  constructor() {
+    super()
+    this.state = {sending: false, sent: false}
+  }
   onSubmit(event) {
     event.preventDefault()
+    this.setState({sending: true})
     fetch('https://bot.kanttiinit.fi/feedback', {
       method: 'POST',
       headers: {
@@ -16,14 +21,20 @@ export default class Contact extends React.Component {
         message: `Email: ${this.refs.email.value}\n"${this.refs.message.value}"`
       })
     })
+    .then(() => setState({sending: false, sent: true}))
+    .catch(() => setState({sending: false, error: true}))
+  }
+  componentDidMount() {
+    this.refs.email.focus()
   }
   render() {
+    const {sending} = this.state;
     return (
       <PageContainer title="Contact">
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form className="contact-form" onSubmit={this.onSubmit.bind(this)}>
           <input type="email" ref="email" placeholder="E-mail" required />
-          <textarea placeholder="Message" rows="4" ref="message" required></textarea>
-          <button type="submit">Lähetä</button>
+          <textarea placeholder="Message" rows="10" ref="message" required></textarea>
+          <button disabled={sending} type="submit">{sending ? 'Lähetetään...' : 'Lähetä'}</button>
         </form>
       </PageContainer>
     )

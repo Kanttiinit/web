d7e312b754bc866d13e8e577acf80415c0e8a9c6
Max Huttunen <max.huttunen@gmail.com>
2016-08-24 20:58:19 +0300

    small bug fixes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@ import React from 'react'
 import loaderImg from '../assets/loader.svg'
 
 const Loader = () => (
-  <div style={{textAlign: 'center', margin: '2rem'}}>
-    <img src={loaderImg} />
+  <div style={{textAlign: 'center', margin: '4rem 1rem'}}>
+    <img width="100" height="100" src={loaderImg} />
   </div>
 )
 

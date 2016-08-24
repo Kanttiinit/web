357c7ecdcfc1bcd82ef1412af55f1a473d4d89fa
Max Huttunen <max.huttunen@gmail.com>
2016-08-24 20:50:32 +0300

    add compression

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 express()
+.use(require('compression')())
 .use(express.static('dist'))
 .get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 .listen(process.env.PORT || 5000);

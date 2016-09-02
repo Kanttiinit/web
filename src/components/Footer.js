7174d36341b87bf51d9fab68a837758396d5e4eb
Max Huttunen <max.huttunen@gmail.com>
2016-09-02 16:39:29 +0300

    fix mobile stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,11 +9,11 @@ const Footer = ({path}) => {
   const getClassName = currentPath => c({current: path === currentPath})
   return (
     <footer className="footer">
-      <a href="/" className={getClassName('/')}><Text id="menus" /></a>&bull;
-      <a href="/settings" className={getClassName('/settings')}><Text id="settings" /></a>&bull;
-      <a href="/contact" className={getClassName('/contact')}><Text id="contact" /></a>&bull;
-      <a href="/privacy-policy" className={getClassName('/privacy-policy')}><Text id="privacyPolicy" /></a>&bull;
-      <a href="/beta" className={getClassName('/beta')}>Beta</a>&bull;
+      <a href="/" className={getClassName('/')}><Text id="menus" /></a>
+      <a href="/settings" className={getClassName('/settings')}><Text id="settings" /></a>
+      <a href="/contact" className={getClassName('/contact')}><Text id="contact" /></a>
+      <a href="/privacy-policy" className={getClassName('/privacy-policy')}><Text id="privacyPolicy" /></a>
+      <a href="/beta" className={getClassName('/beta')}>Beta</a>
       <a href="https://github.com/Kanttiinit" target="_blank"><Text id="sourceCode" /></a>
       <AppLinks style={{marginTop: '2rem'}} />
     </footer>

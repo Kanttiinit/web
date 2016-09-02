db7a47488c6c65dfcfdb557db5e2c3105611f185
Jere Vaara <jerevaara@gmail.com>
2016-09-02 23:03:02 +0300

    beta buttons ui changes

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -52,11 +52,11 @@ export default {
     en: 'closed'
   },
   androidTester: {
-    fi: 'Ryhdy Android testaajaksi',
+    fi: 'Ryhdy Android-applikaation betatestaajaksi',
     en: 'Become a tester on Android'
   },
   betaSite: {
-    fi: 'Kokeile beta sivua',
+    fi: 'Kokeile sivun betaversiota',
     en: 'Try the beta site'
   },
   useLocation: {

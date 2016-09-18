5e6030293d91ffac80c5daa5f3d7b38d39605b6b
Max Huttunen <max.huttunen@gmail.com>
2016-09-19 00:01:00 +0300

    add put method to http api

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,19 +3,23 @@ import 'isomorphic-fetch';
 const API_BASE = 'https://kitchen.kanttiinit.fi'
 
 export default {
-	fetch(method, url) {
+	fetch(method, url, body) {
 		const headers = {}
 		if (this.token) {
 			headers.Authorization = this.token
 		}
 		return fetch(API_BASE + url, {
-			method: 'GET',
+			method,
+			body: body ? JSON.stringify(body) : undefined,
 			headers
 		})
 		.then(r => r.json())
 	},
 	get(url, lang) {
 		return this.fetch('GET', url + '?&lang=' + lang)
+	},
+	put(url, data) {
+		return this.fetch('PUT', url, data)
 	},
 	setToken(token) {
 		this.token = token

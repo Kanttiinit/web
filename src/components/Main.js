d25538e9d99335f61de584170042a86538350641
Max Huttunen <max.huttunen@gmail.com>
2016-09-19 00:10:23 +0300

    refactor GA out of index.html

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,15 @@ import {Provider} from 'react-redux'
 import page from 'page'
 import key from 'keymaster'
 import http from '../utils/http'
+import GA from 'react-ga'
 
 import store from '../store'
 import {setToken} from '../store/actions/preferences'
 import {setView, setDayOffset, closeModal} from '../store/actions/values'
-
 import parseAuth from '../utils/parseAuth'
 import App from './App'
+
+// import all views
 import Menus from './Menus'
 import PrivacyPolicy from './PrivacyPolicy'
 import Contact from './Contact'
@@ -20,6 +22,20 @@ import NotFound from './NotFound'
 
 window.isBeta = location.hostname === 'beta.kanttiinit.fi' || location.hostname === 'localhost'
 
+// keyboard shortcuts
+key('left,right', (event, handler) => {
+  const offset = handler.shortcut === 'left' ? -1 : 1
+  store.dispatch(setDayOffset(store.getState().value.dayOffset + offset))
+})
+
+key('esc', () => store.dispatch(closeModal()))
+
+// analytics setup
+GA.initialize('UA-55969084-5', {
+  debug: process.env.NODE_ENV !== 'production'
+})
+
+// routing
 const routes = {
   '/': Menus,
   '/privacy-policy': PrivacyPolicy,
@@ -28,13 +44,6 @@ const routes = {
   '/settings': Settings,
   '*': NotFound
 }
-
-key('left,right', (event, handler) => {
-  const offset = handler.shortcut === 'left' ? -1 : 1
-  store.dispatch(setDayOffset(store.getState().value.dayOffset + offset))
-})
-
-key('esc', () => store.dispatch(closeModal()))
 
 Object.keys(routes).forEach(path => {
   page(path, () => {
@@ -46,10 +55,13 @@ Object.keys(routes).forEach(path => {
       view: React.createElement(routes[path])
     }
     store.dispatch(setView(route))
+    GA.set({page: path})
+    GA.pageview(path)
   })
 })
 page()
 
+// login if auth token is in URL
 const auth = parseAuth(page)
 if (auth) {
   http.get(`/me/login?${auth.provider}Token=${auth.token}`)
@@ -58,8 +70,9 @@ if (auth) {
   })
 }
 
+// export app wrapped in store provider
 export default () => (
   <Provider store={store}>
      <App />
   </Provider>
- )
+)

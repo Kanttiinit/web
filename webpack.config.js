a55cae179b94f150305f4b4d3a5fff5aa431975c
Max Huttunen <max.huttunen@gmail.com>
2016-09-22 23:48:41 +0300

    fix webpack bug

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const prodPlugins = [
   new webpack.optimize.DedupePlugin()
 ];
 
-const plugins = [
+let plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify(process.env.NODE_ENV)

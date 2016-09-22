d939a242924d13852b9a3b214dbaea217ba24224
Max Huttunen <max.huttunen@gmail.com>
2016-09-22 23:18:30 +0300

    add web manifest

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require('express');
 express()
 .use(require('compression')())
 .use(express.static('dist'))
+.use(express.static(__dirname + '/src/assets'))
 .get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
-.listen(process.env.PORT || 8080);
+.listen(process.env.PORT || 5000);

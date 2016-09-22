5f703953031fca1e0ee6f74205319d7b554e7974
Max Huttunen <max.huttunen@gmail.com>
2016-09-22 23:50:30 +0300

    fix more

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const prodPlugins = [
   new webpack.optimize.DedupePlugin()
 ];
 
-let plugins = [
+var plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify(process.env.NODE_ENV)

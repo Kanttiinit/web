2fef7b61b5428ca3bfd3a65f0b8120175f2087f8
Max Huttunen <max.huttunen@gmail.com>
2016-09-25 15:58:40 +0300

    redirect to non-www

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
 express()
+.use((req, res, next) => {
+  if (req.headers.host.slice(0, 4) === 'www.') {
+    res.redirect(301, `${req.protocol}://${req.headers.host.slice(4)}${req.originalUrl}`);
+  } else {
+    next();
+  }
+})
 .use(require('compression')())
 .use(express.static('dist'))
 .use(express.static(__dirname + '/src/assets'))

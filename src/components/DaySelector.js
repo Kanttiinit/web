9e1522ade0a022136e629ca1398b066d7dd4df2c
Max Huttunen <max.huttunen@gmail.com>
2017-08-10 18:46:16 +0300

    fix bug when changing day in modal

--- src/components/DaySelector.js
+++ src/components/DaySelector.js
@@ -12,6 +12,7 @@ import Text from './Text'
 
 @observer
 export default class DaySelector extends React.PureComponent {
+
   focus = (e: Element) => {
     if (e) {
       const node = ReactDOM.findDOMNode(e)
@@ -23,17 +24,19 @@ export default class DaySelector extends React.PureComponent {
 
   render() {
     const date = uiState.day
+    const now = moment()
     return (
       <div className={css.days}>
         {times(6, i => {
           const day = moment().add({day: i})
           const active = date.isSame(day, 'day')
+          const search = now.isSame(day, 'day') ? '' : `?day=${day.format('YYYY-MM-DD')}`
           return (
             <Link
               key={i}
               ref={active && this.focus}
               className={active ? css.selected : ''}
-              to={{pathname: '/', search: `?day=${day.format('YYYY-MM-DD')}`}}>
+              to={{pathname: location.pathname, search}}>
               <Text moment={day} id="dd D.M." />
             </Link>
           )

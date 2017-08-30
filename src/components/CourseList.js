d8009477ba58658b704566e5806ebb7994b0efd1
Jere Vaara <jerevaara@gmail.com>
2017-08-30 10:43:29 +0300

    Course grouping in menus

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -2,10 +2,34 @@
 import React from 'react'
 import Heart from 'react-icons/lib/io/heart'
 import c from 'classnames'
+import _ from 'lodash'
 
 import type {CourseType} from '../store/types'
 import Text from './Text'
 import css from '../styles/CourseList.scss'
+
+const prefixer = (title: string) => {
+  const split = _.split(title, ':', 2)
+  return split.length > 1 ? _.get(split, ['0'], '') : ''
+}
+
+const groupByPrefix = (courses: Array<CourseType>): Array<any> =>
+  _.groupBy(courses, c => prefixer(c.title))
+
+const courseGroups = (courses: Array<CourseType>) => 
+  _.values(_.mapValues(groupByPrefix(courses), (group, groupKey) => ({
+    key: groupKey,
+    courses: group.map(c => ({ ...c, title: c.title.replace(groupKey + ': ', '') }))
+  })))
+
+const Course = ({course}: {course: CourseType}) => (
+  <div
+    className={c(css.course, course.isFavorite && css.favoriteCourse)}>
+    {course.isFavorite && <Heart className={`inline-icon ${css.icon}`} />}
+    <span className={css.title}>{course.title}</span>
+    <span className={css.props}>{course.properties.join(' ')}</span>
+  </div>
+)
 
 export default class CourseList extends React.PureComponent {
   props: {
@@ -18,15 +42,12 @@ export default class CourseList extends React.PureComponent {
     return (
       <div {...props}>
         {!courses.length && <Text id="noMenu" element="span" className={css.emptyText} />}
-        {courses.map((course, i) => 
-          <div
-            className={c(css.course, course.isFavorite && css.favoriteCourse)}
-            key={i}>
-            {course.isFavorite && <Heart className={`inline-icon ${css.icon}`} />}
-            <span className={css.title}>{course.title}</span>
-            <span className={css.props}>{course.properties.join(' ')}</span>
+        {courseGroups(courses).map(group => (
+          <div className={css.courseGroup}>
+            <span className={css.courseGroupTitle}>{ _.capitalize(group.key) }</span>
+            {group.courses.map((c, i) => <Course key={i} course={c} />)}
           </div>
-        )}
+        ))}
       </div>
     )
   }

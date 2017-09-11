7e5add4b354330ace822070bdd00cccb65836d3e
Max Huttunen <max.huttunen@gmail.com>
2017-09-11 14:14:41 +0300

    revisit clients file

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -8,17 +8,22 @@ export default class Clients extends React.PureComponent {
   render() {
     return (
       <PageContainer title={<Text id="otherClients" />}>
-        <ul>
-          <li>
-            <a href="https://telegram.me/KanttiinitBOT" target="_blank">Telegram Chat Bot</a>
-          </li>
-          <li>
-            <a href="https://github.com/Kanttiinit/cli" target="_blank">Command Line Interface</a>
-          </li>
-          <li>
-            <a href="https://play.google.com/apps/testing/com.kanttiinit" target="_blank">Android App (not actively maintained)</a>
-          </li>
-        </ul>
+        <h3>
+          <a href="https://folio.kanttiinit.fi/">Kanttiinit Folio</a>
+        </h3>
+        <p>JavaScript-less version of Kanttiinit for people with tin foil hats.</p>
+        <h3>
+          <a href="https://telegram.me/KanttiinitBOT" target="_blank">Kanttiinit Telegram</a>
+        </h3>
+        <p>Chat bot for Telegram.</p>
+        <h3>
+          <a href="https://github.com/Kanttiinit/cli" target="_blank">Kanttiinit CLI</a>
+        </h3>
+        <p>Command line interface built with Node.</p>
+        <h3>
+          <a href="https://play.google.com/apps/testing/com.kanttiinit" target="_blank">Kanttiinit Android</a>
+        </h3>
+        <p>Android App, but not actively maintained anymore.</p>
       </PageContainer>
     )
   }   

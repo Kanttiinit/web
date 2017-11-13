4ab89dd97bbaf3ac627d67c3919636833876ed23
Max Huttunen <max.huttunen@gmail.com>
2017-11-13 15:12:12 +0200

    properties can be selected and are persisted

--- src/components/PropertySelector/PropertySelector.js
+++ src/components/PropertySelector/PropertySelector.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import {observer} from 'mobx-react'
 import c from 'classnames'
 
+import {preferenceStore} from '../../store'
 import css from './PropertySelector.scss'
 
 const properties = ['A', 'G', 'M', 'S', 'T', 'V', 'Veg']
 
-export default class PropertySelector extends React.Component {
-  render() {
-    return (
-      <div className={css.container}>
-        {properties.map(p =>
-        <button className={c('button', css.property)} key={p}>{p}</button>
-        )}
-      </div>
-    )
-  }
-}
+export default observer(() =>
+  <div className={css.container}>
+    {properties.map(p =>
+    <button
+      onClick={() => preferenceStore.toggleProperty(p)}
+      className={c('button', css.property, preferenceStore.isPropertySelected(p) && css.selected)}
+      key={p}>
+      {p}
+    </button>
+    )}
+  </div>
+)

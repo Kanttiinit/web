9bc1c82fceeff32ab9e38b408fcd968f91b6f488
Max Huttunen <max.huttunen@gmail.com>
2017-12-26 13:01:35 +0200

    add animation to restaurant placeholders

--- src/components/Restaurant/PlaceHolder.tsx
+++ src/components/Restaurant/PlaceHolder.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import {times, random} from 'lodash'
+import * as classnames from 'classnames'
 
 const css = require('./Restaurant.scss')
 
@@ -9,7 +10,7 @@ export default class Placeholder extends React.Component {
   }
   render() {
     return (
-      <div className={css.container + ' ' + css.placeholder}>
+      <div className={classnames(css.container, css.placeholder)}>
         <div className={css.header} style={{width: random(30, 40) + '%'}}></div>
         <div className={css.body}>
           {times(10, i => <div key={i} className={css.course} style={{width: random(40, 100) + '%'}}></div>)}

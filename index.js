8986a82e7eef7aa39dd6a7812154f0c8815b9d19
Max Huttunen <max.huttunen@gmail.com>
2018-01-21 18:12:17 +0200

    add endpoint for checking version

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const semver = require('semver')
+const pkg = require('./package.json')
 
 express()
 .use((req, res, next) => {
@@ -12,5 +14,12 @@ express()
 .use(express.static('dist'))
 .use(express.static(__dirname + '/src/assets'))
 .get('/admin', (req, res) => res.sendFile(__dirname + '/dist/index_admin.html'))
+.get('/check-update', (req, res) => {
+  try {
+    res.json({updateAvailable: semver.gt(pkg.version, req.query.version)})
+  } catch (e) {
+    res.status(400).json({error: true})
+  }
+})
 .get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 .listen(process.env.PORT || 8080)

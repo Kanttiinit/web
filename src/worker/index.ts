14ab3566774b9eea285fd73b04b0686d3b5dd234
Max Huttunen <max.huttunen@gmail.com>
2018-01-25 14:51:25 +0200

    make service worker work

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,33 +1,48 @@
-(async () => {
-  const global = (self as any) as ServiceWorkerGlobalScope
+import {version} from '../utils/consts'
 
-  const CACHE_NAME = 'cache-v2'
-  const urlsToCache = [
-    '/',
-    '/app.js'
-  ]
-  const cache = await caches.open(CACHE_NAME)
-  
-  const resolve = async (request: Request) => {
-    if (request.url.match(/^https:\/\/kitchen\.kanttiinit\.fi/)) {
-      try {
-        const response = await fetch(request)
-        cache.put(request, response.clone())
-        return response
-      } catch (e) {}
-      return caches.match(request)
-    } else if (request.url.match(/\.(png|jpg)$/)) {
-      cache.add(request)
-    }
-    return (await caches.match(request)) || fetch(request)
+const worker = (self as any) as ServiceWorkerGlobalScope
+
+const CACHE_NAME = `cache-${version}`
+const urlsToCache = [
+  '/',
+  '/app.js',
+  '/fi.png',
+  '/en.png',
+  '/logo_48.png',
+  '/locating.svg',
+  '/fonts/Interface-Regular.woff2',
+  '/fonts/Interface-Medium.woff2',
+  '/fonts/Interface-Bold.woff2'
+]
+
+let cache: Cache
+const getCache = async () => cache || (cache = await caches.open(CACHE_NAME))
+
+const resolve = async (request: Request) => {
+  const cache = await getCache()
+  if (request.url.match(/^https:\/\/kitchen\.kanttiinit\.fi/)) {
+    try {
+      const response = await fetch(request)
+      cache.put(request, response.clone())
+      return response
+    } catch (e) {}
+    return caches.match(request)
+  } else if (request.url.match(/amazonaws.+\.(png|jpg)$/)) {
+    cache.add(request)
   }
-  
-  global.addEventListener('install', (event: ExtendableEvent) => {
-    global.skipWaiting()
-    event.waitUntil(cache.addAll(urlsToCache));
-  })
-  
-  global.addEventListener('fetch', (event: FetchEvent) => {
-    event.respondWith(resolve(event.request))
-  })
-})()
+  return (await caches.match(request)) || fetch(request)
+}
+
+const install = async () => {
+  const cache = await getCache()
+  await cache.addAll(urlsToCache)
+}
+
+worker.addEventListener('install', (event: ExtendableEvent) => {
+  worker.skipWaiting()
+  event.waitUntil(install())
+})
+
+worker.addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(resolve(event.request))
+})

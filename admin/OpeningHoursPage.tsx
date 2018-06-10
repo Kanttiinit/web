137e74a70cbb9e0f48b87b566ff6d2d3efff951a
Max Huttunen <max.huttunen@gmail.com>
2018-06-10 16:36:49 +0300

    table works better

--- admin/OpeningHoursPage.tsx
+++ admin/OpeningHoursPage.tsx
@@ -5,14 +5,14 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Dialog from '@material-ui/core/Dialog';
-import { withStyles } from '@material-ui/core/styles';
 import * as sortBy from 'lodash/fp/sortBy';
 import { RestaurantType, Lang } from '../src/store/types';
 import * as api from '../src/utils/api';
@@ -32,185 +32,175 @@ type ListState = {
   modalItem?: any;
 };
 
-const styles = theme => ({
-  gridContainer: {
-    marginTop: theme.spacing.unit
+const days = [0, 1, 2, 3, 4, 5, 6];
+
+class OpeningHoursList extends React.PureComponent<any, ListState> {
+  state: ListState = {
+    loading: false,
+    modalOpen: false,
+    modalMode: 'create'
+  };
+
+  componentDidMount() {
+    this.fetchOpeningHours();
   }
-});
 
-const OpeningHoursList = withStyles(styles)(
-  class extends React.PureComponent<any, ListState> {
-    state: ListState = {
-      loading: false,
-      modalOpen: false,
-      modalMode: 'create'
-    };
-
-    componentDidMount() {
+  componentDidUpdate(prevProps) {
+    if (prevProps.restaurantId !== this.props.restaurantId) {
       this.fetchOpeningHours();
     }
+  }
 
-    componentDidUpdate(prevProps) {
-      if (prevProps.restaurantId !== this.props.restaurantId) {
-        this.fetchOpeningHours();
-      }
-    }
+  fetchOpeningHours = async () => {
+    this.setState({ loading: true });
+    this.setState({
+      loading: false,
+      openingHours: await http.get(
+        `/admin/opening-hours/${this.props.restaurantId}`,
+        true
+      )
+    });
+  };
 
-    fetchOpeningHours = async () => {
-      this.setState({ loading: true });
-      this.setState({
-        loading: false,
-        openingHours: await http.get(
-          `/admin/opening-hours/${this.props.restaurantId}`,
-          true
-        )
-      });
-    };
+  openCreateModal = () => {
+    this.setState({ modalOpen: true, modalMode: 'create' });
+  };
 
-    openCreateModal = () => {
-      this.setState({ modalOpen: true, modalMode: 'create' });
-    };
+  closeModal = () => this.setState({ modalOpen: false });
 
-    closeModal = () => this.setState({ modalOpen: false });
-
-    handleModalAction = async (item: any) => {
-      if (item) {
-        if (this.state.modalMode === 'create') {
-          await http.post('/admin/openinghours', {
-            ...item,
-            RestaurantId: this.props.restaurantId
-          });
-          await this.fetchOpeningHours();
-        }
-      }
-      this.closeModal();
-    };
-
-    editItem = item => {
-      this.setState({ modalOpen: true, modalMode: 'edit', modalItem: item });
-    };
-
-    deleteItem = async id => {
-      if (confirm('Are you sure?')) {
-        await http.delete(`/admin/openinghours/${id}`);
+  handleModalAction = async (item: any) => {
+    if (item) {
+      if (this.state.modalMode === 'create') {
+        await http.post('/admin/openinghours', {
+          ...item,
+          RestaurantId: this.props.restaurantId
+        });
         await this.fetchOpeningHours();
       }
-    };
-
-    renderGridItem = (day: number, item: any) => {
-      return (
-        <Grid md key={day} item>
-          <Card>
-            <CardContent>
-              <Typography variant="body2">
-                {moment(item.dayOfWeek + 1, 'E').format('ddd')}
-                &nbsp;
-                {item.closed ? (
-                  'Closed'
-                ) : (
-                  <span>
-                    {item.opens} &ndash; {item.closes}
-                  </span>
-                )}
-              </Typography>
-              <Typography color="textSecondary">
-                From: {item.from}
-                <br />To: {item.to || 'forever'}
-              </Typography>
-            </CardContent>
-            {item.manualEntry && (
-              <CardActions>
-                <Button
-                  size="small"
-                  color="secondary"
-                  onClick={() => this.deleteItem(item.id)}
-                >
-                  Delete
-                </Button>
-                <Button
-                  size="small"
-                  color="primary"
-                  onClick={() => this.editItem(item)}
-                >
-                  Edit
-                </Button>
-              </CardActions>
-            )}
-          </Card>
-        </Grid>
-      );
-    };
-
-    render() {
-      const {
-        loading,
-        openingHours,
-        modalOpen,
-        modalMode,
-        modalItem
-      } = this.state;
-
-      if (loading) {
-        return <Progress />;
-      }
-
-      const { classes } = this.props;
-      const groupedHours = groupBy('dayOfWeek', openingHours);
-      let grid = [];
-      for (let row = 0; row < 10000; row++) {
-        let foundItems = 0;
-        grid.push(
-          <Grid
-            className={classes.gridContainer}
-            wrap="nowrap"
-            key={row}
-            container
-            spacing={8}
-          >
-            {[0, 1, 2, 3, 4, 5, 6].map(day => {
-              const item = groupedHours[day] && groupedHours[day][row];
-              if (!item) {
-                return <Grid item md key={day} />;
-              }
-              foundItems++;
-              return this.renderGridItem(day, item);
-            })}
-          </Grid>
-        );
-        if (foundItems === 0) {
-          break;
-        }
-      }
-
-      return (
-        <React.Fragment>
-          <br />
-          <br />
-          <Button
-            onClick={this.openCreateModal}
-            variant="raised"
-            color="primary"
-          >
-            Create new
-          </Button>
-          {grid}
-          <Dialog
-            fullWidth
-            maxWidth="sm"
-            onClose={this.closeModal}
-            open={modalOpen}
-          >
-            <OpeningHoursEditor
-              mode={modalMode}
-              onAction={this.handleModalAction}
-              item={modalItem}
-            />
-          </Dialog>
-        </React.Fragment>
-      );
     }
+    this.closeModal();
+  };
+
+  editItem = item => {
+    this.setState({ modalOpen: true, modalMode: 'edit', modalItem: item });
+  };
+
+  deleteItem = async id => {
+    if (confirm('Are you sure?')) {
+      await http.delete(`/admin/openinghours/${id}`);
+      await this.fetchOpeningHours();
+    }
+  };
+
+  renderGridItem = (day: number, item: any) => {
+    return (
+      <TableCell padding="none" key={day}>
+        <Typography variant="body2">
+          {item.closed ? (
+            'Closed'
+          ) : (
+            <span>
+              {item.opens} &ndash; {item.closes}
+            </span>
+          )}
+        </Typography>
+        <Typography color="textSecondary">
+          From: {item.from}
+          <br />To: {item.to || 'forever'}
+        </Typography>
+        {item.manualEntry && (
+          <React.Fragment>
+            <Button
+              size="small"
+              color="secondary"
+              onClick={() => this.deleteItem(item.id)}
+            >
+              Delete
+            </Button>
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => this.editItem(item)}
+            >
+              Edit
+            </Button>
+          </React.Fragment>
+        )}
+      </TableCell>
+    );
+  };
+
+  render() {
+    const {
+      loading,
+      openingHours,
+      modalOpen,
+      modalMode,
+      modalItem
+    } = this.state;
+
+    if (loading) {
+      return <Progress />;
+    }
+
+    const groupedHours = groupBy('dayOfWeek', openingHours);
+    let rows = [];
+    for (let row = 0; row < 100; row++) {
+      let foundItems = 0;
+      const rowContent = (
+        <TableRow key={row}>
+          {days.map(day => {
+            const item = groupedHours[day] && groupedHours[day][row];
+            if (!item) {
+              return <TableCell padding="none" key={day} />;
+            }
+            foundItems++;
+            return this.renderGridItem(day, item);
+          })}
+        </TableRow>
+      );
+      if (foundItems === 0) {
+        break;
+      } else {
+        rows.push(rowContent);
+      }
+    }
+
+    return (
+      <React.Fragment>
+        <br />
+        <br />
+        <Button onClick={this.openCreateModal} variant="raised" color="primary">
+          Create new
+        </Button>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {days.map(d => (
+                <TableCell padding="none" key={d}>
+                  {moment(d + 1, 'E').format('ddd')}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>{rows}</TableBody>
+        </Table>
+        <Dialog
+          fullWidth
+          maxWidth="sm"
+          onClose={this.closeModal}
+          open={modalOpen}
+        >
+          <OpeningHoursEditor
+            mode={modalMode}
+            onAction={this.handleModalAction}
+            item={modalItem}
+          />
+        </Dialog>
+      </React.Fragment>
+    );
   }
-);
+}
 
 type State = {
   restaurants?: Array<RestaurantType>;

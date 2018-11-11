ed0a57a2dd0150a6a616fcd4042dc5db0ede732d
Max Huttunen <max.huttunen@gmail.com>
2018-11-11 15:22:25 +0200

    better tooltip

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,30 +1,78 @@
-import * as React from 'react';
-
-import * as styles from './Tooltip.scss';
-import translations from '../../utils/translations';
-import { preferenceStore } from '../../store';
 import { observer } from 'mobx-react';
+import Popper from 'popper.js';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { preferenceStore } from '../../store';
+import translations from '../../utils/translations';
+import * as styles from './Tooltip.scss';
 
 type Props = {
   children: React.ReactNode;
   text?: string;
   translationKey?: keyof (typeof translations); // eslint-disable-line
-  position?: 'up' | 'left' | 'right' | 'down';
+  position?: Popper.Position;
 };
 
-const Tooltip = observer(
-  ({ text, translationKey, children, position = 'down' }: Props) => {
-    const title = text || translations[translationKey][preferenceStore.lang];
+type State = {
+  isOpen: boolean;
+};
+
+@observer
+class Tooltip extends React.Component<Props, State> {
+  state = { isOpen: false };
+  anchor: Element;
+  tooltip: Element;
+  popper: Popper;
+
+  saveAnchorRef = (e: Element) => {
+    if (e) {
+      this.anchor = e;
+      this.updatePopper();
+    }
+  };
+
+  saveTooltipRef = (e: Element) => {
+    if (e) {
+      this.tooltip = e;
+      this.updatePopper();
+    }
+  };
+
+  updatePopper = () => {
+    if (this.anchor && this.tooltip) {
+      this.popper = new Popper(this.anchor, this.tooltip, {
+        placement: this.props.position || 'bottom-start'
+      });
+    }
+  };
+
+  open = () => this.setState({ isOpen: true });
+
+  close = () => this.setState({ isOpen: false });
+
+  render() {
+    const contents =
+      this.props.text ||
+      translations[this.props.translationKey][preferenceStore.lang];
     return (
-      <span
-        className={styles.tooltip}
-        data-title={title}
-        data-tooltip-pos={position}
-      >
-        {children}
-      </span>
+      <React.Fragment>
+        <span
+          onMouseOver={this.open}
+          onMouseOut={this.close}
+          ref={this.saveAnchorRef}
+        >
+          {this.props.children}
+        </span>
+        {this.state.isOpen &&
+          ReactDOM.createPortal(
+            <div className={styles.tooltip} ref={this.saveTooltipRef}>
+              {contents}
+            </div>,
+            document.body
+          )}
+      </React.Fragment>
     );
   }
-);
+}
 
 export default Tooltip;

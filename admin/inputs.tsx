1c4d4de7cb4058462ca9ed7ff9fcd97e3d4cff92
Max Huttunen <max.huttunen@gmail.com>
2019-02-24 19:32:24 +0200

    re-use report modal components in admin

--- admin/inputs.tsx
+++ admin/inputs.tsx
@@ -1,39 +1,36 @@
-import * as React from 'react';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import Grid from '@material-ui/core/Grid';
-import Input from '@material-ui/core/Input';
-import MaskedInput from 'react-text-mask';
+import * as format from 'date-fns/format';
 import * as get from 'lodash/fp/get';
 import * as sortBy from 'lodash/fp/sortBy';
-import * as setIsoDay from 'date-fns/set_iso_day';
-import * as format from 'date-fns/format';
-import PigeonMap from 'pigeon-maps';
-import Marker from 'pigeon-marker';
+import * as React from 'react';
 
+import LatLngInput from '../src/components/LatLngInput';
+import OpeningHoursInput from '../src/components/OpeningHoursInput';
+import useResource from '../src/utils/useResource';
 import * as api from './api';
 import models from './models';
 import { ModelField, ModelFieldGroup, RelationField } from './models';
 
-type InputProps = {
+interface InputProps {
   value: any;
   field: ModelField;
   setValue(path: string, value: any): any;
-};
+}
 
-type GroupInputProps = {
-  value: Array<any>;
+interface GroupInputProps {
+  value: any[];
   field: ModelFieldGroup;
   setValue(path: string, value: any): any;
-};
+}
 
 const UrlInput = ({ value, setValue, field }: InputProps) => (
   <TextField
@@ -100,43 +97,38 @@ class AddressInput extends React.PureComponent {
   }
 }
 
-class RegExpInput extends React.PureComponent {
-  props: InputProps;
-  state = { test: '' };
-
-  render() {
-    const { field, value, setValue } = this.props;
-    const { test } = this.state;
-    const match = !!test.match(new RegExp(value));
-    return (
-      <Grid container spacing={24}>
-        <Grid item sm>
-          <TextField
-            fullWidth
-            label={field.title}
-            onChange={e => setValue(field.path, e.target.value)}
-            value={value || ''}
-            type="text"
-          />
-        </Grid>
-        <Grid item sm>
-          <TextField
-            fullWidth
-            label="Test value"
-            value={test}
-            error={!match}
-            onChange={e => this.setState({ test: e.target.value })}
-            helperText={
-              match
-                ? 'The regexp matches this value.'
-                : 'The regexp does not match this value.'
-            }
-          />
-        </Grid>
+const RegExpInput = (props: InputProps) => {
+  const { field, value, setValue } = props;
+  const [test, setTest] = React.useState('');
+  const match = !!test.match(new RegExp(value));
+  return (
+    <Grid container spacing={24}>
+      <Grid item sm>
+        <TextField
+          fullWidth
+          label={field.title}
+          onChange={e => setValue(field.path, e.target.value)}
+          value={value || ''}
+          type="text"
+        />
       </Grid>
-    );
-  }
-}
+      <Grid item sm>
+        <TextField
+          fullWidth
+          label="Test value"
+          value={test}
+          error={!match}
+          onChange={e => setTest(e.target.value)}
+          helperText={
+            match
+              ? 'The regexp matches this value.'
+              : 'The regexp does not match this value.'
+          }
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
 export const BooleanInput = ({ value, setValue, field }: InputProps) => (
   <FormControlLabel
@@ -172,64 +164,32 @@ export const DateInput = ({ value, setValue, field }: InputProps) => (
   />
 );
 
-const Map = (props: any) => {
-  const onChange = ({ latLng }: { latLng: [number, number] }) =>
-    props.onChange(latLng[0], latLng[1]);
+const LocationInput = (props: GroupInputProps) => {
+  if (
+    typeof props.value[0] !== 'number' ||
+    typeof props.value[0] !== 'number'
+  ) {
+    return null;
+  }
   return (
-    <PigeonMap
-      onClick={onChange}
-      defaultCenter={[props.latitude || 60.1705, props.longitude || 24.9414]}
-      defaultZoom={14}
-    >
-      {props.latitude &&
-        props.longitude && (
-          <Marker anchor={[props.latitude, props.longitude]} />
-        )}
-    </PigeonMap>
+    <LatLngInput
+      value={props.value as [number, number]}
+      onChange={value => {
+        props.setValue('latitude', value[0]);
+        props.setValue('longitude', value[1]);
+      }}
+    />
   );
 };
-
-const LocationInput = (props: GroupInputProps) => (
-  <FormControl fullWidth>
-    <FormGroup>
-      <Grid container spacing={24}>
-        <Grid item xs>
-          <NumericInput
-            setValue={(f, v) => props.setValue(f, v)}
-            value={props.value[0]}
-            field={props.field.fields[0]}
-          />
-        </Grid>
-        <Grid item xs>
-          <NumericInput
-            setValue={(f, v) => props.setValue(f, v)}
-            value={props.value[1]}
-            field={props.field.fields[1]}
-          />
-        </Grid>
-      </Grid>
-      <br />
-      <div style={{ height: 200 }}>
-        <Map
-          latitude={props.value[0]}
-          longitude={props.value[1]}
-          onChange={(lat: number, lon: number) =>
-            props.setValue('latitude', lat) || props.setValue('longitude', lon)
-          }
-        />
-      </div>
-    </FormGroup>
-  </FormControl>
-);
 
 const TranslatedInput = (props: GroupInputProps) => (
   <Grid container spacing={24}>
     {props.field.fields.map((field, i) => (
       <Grid key={i} item md>
         {React.createElement(inputs[field.type] || inputs._, {
-          value: props.value[i],
+          field: { ...field, title: `${props.field.title} in ${field.title}` },
           setValue: props.setValue,
-          field: { ...field, title: `${props.field.title} in ${field.title}` }
+          value: props.value[i]
         })}
       </Grid>
     ))}
@@ -274,56 +234,38 @@ export const DayOfWeekSelect = (props: InputProps) => (
   </FormControl>
 );
 
-class RelationInput extends React.PureComponent {
-  props: {
-    value: any;
-    field: RelationField;
-    setValue(path: string, value: any): any;
-  };
+const RelationInput = (props: {
+  value: any;
+  field: RelationField;
+  setValue(path: string, value: any): any;
+}) => {
+  const { value, field, setValue } = props;
+  const [state, fetch] = useResource(null, true);
 
-  state: {
-    items?: Array<any>;
-    loading?: boolean;
-  } = {
-    loading: true
-  };
+  React.useEffect(() => {
+    const model = models.find(m => m.key === props.field.relationKey);
+    fetch(api.fetchItems(model));
+  }, []);
 
-  async fetchItems() {
-    const model = models.find(m => m.key === this.props.field.relationKey);
-    this.setState({ loading: true });
-    this.setState({
-      items: await api.fetchItems(model),
-      loading: false
-    });
+  if (state.pending) {
+    return <span>Loading...</span>;
   }
-
-  componentDidMount() {
-    this.fetchItems();
-  }
-
-  render() {
-    const { value, field, setValue } = this.props;
-    const { items, loading } = this.state;
-    if (loading) {
-      return <span>Loading...</span>;
-    }
-    return (
-      <FormControl fullWidth>
-        <InputLabel>{field.title}</InputLabel>
-        <Select
-          value={value || (items.length ? items[0].id : '')}
-          onChange={e => setValue(field.path, e.target.value)}
-        >
-          {sortBy(field.relationDisplayField, items).map((item: any) => (
-            <MenuItem key={item.id} value={item.id}>
-              {get(field.relationDisplayField, item)}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    );
-  }
-}
+  return (
+    <FormControl fullWidth>
+      <InputLabel>{field.title}</InputLabel>
+      <Select
+        value={value || (state.data.length ? state.data[0].id : '')}
+        onChange={e => setValue(field.path, e.target.value)}
+      >
+        {sortBy(field.relationDisplayField, state.data).map((item: any) => (
+          <MenuItem key={item.id} value={item.id}>
+            {get(field.relationDisplayField, item)}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
 
 export const PlainField = ({ value, field, setValue }: InputProps) => (
   <TextField
@@ -335,94 +277,9 @@ export const PlainField = ({ value, field, setValue }: InputProps) => (
   />
 );
 
-type OpeningHoursEditorState = {
-  hours: Array<[number, number] | null>;
-};
-
-class OpeningHoursEditor extends React.PureComponent<
-  InputProps,
-  OpeningHoursEditorState
-> {
-  state: OpeningHoursEditorState = { hours: [] };
-  static getDerivedStateFromProps(
-    props: InputProps,
-    state: OpeningHoursEditorState
-  ) {
-    if (!state.hours.length || state.hours.every(h => h === null)) {
-      return { hours: props.value || props.field.default };
-    }
-    return null;
-  }
-
-  onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const target = event.target;
-    const [open, close] = target.value.split(' – ');
-    this.setState(state => {
-      const hours = [...state.hours];
-      if (open === 'XX:XX' && close === 'XX:XX') {
-        hours[Number(target.name)] = null;
-      } else if (!open.includes('X') && !close.includes('X')) {
-        hours[Number(target.name)] = [
-          Number(open.replace(':', '')),
-          Number(close.replace(':', ''))
-        ];
-      } else {
-        return null;
-      }
-      this.props.setValue(this.props.field.path, hours);
-      return { hours };
-    });
-  };
-
-  serializeHours = (hours: [number, number] | null) =>
-    hours ? `${hours[0]} - ${hours[1]}` : '';
-
-  render() {
-    const { hours } = this.state;
-    return (
-      <div>
-        {(hours || this.props.field.default).map(
-          (hours: [number, number], i: number) => (
-            <FormControl margin="dense" key={i}>
-              <InputLabel shrink htmlFor={`hours${i}`}>
-                {format(setIsoDay(new Date(), i + 1), 'ddd')}
-              </InputLabel>
-              <Input
-                id={`hours${i}`}
-                onChange={this.onChange}
-                name={String(i)}
-                value={this.serializeHours(hours)}
-                inputComponent={({ inputRef, ...props }) => (
-                  <MaskedInput
-                    {...props}
-                    ref={inputRef}
-                    mask={[
-                      /[0-9]{1,2}/,
-                      /[0-9]{1,2}/,
-                      ':',
-                      /[0-9]{1,2}/,
-                      /[0-9]{1,2}/,
-                      ' ',
-                      '–',
-                      ' ',
-                      /[0-9]{1,2}/,
-                      /[0-9]{1,2}/,
-                      ':',
-                      /[0-9]{1,2}/,
-                      /[0-9]{1,2}/
-                    ]}
-                    showMask
-                    placeholderChar="X"
-                  />
-                )}
-              />
-            </FormControl>
-          )
-        )}
-      </div>
-    );
-  }
-}
+const OpeningHoursEditor = ({ value, field, setValue }: InputProps) => (
+  <OpeningHoursInput defaultValue={value || []} onChange={() => {}} />
+);
 
 const inputs: any = {
   url: UrlInput,
---
+++ src/components/OpeningHoursInput.tsx
@@ -0,0 +1,136 @@
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import CopyFromPrevious from '@material-ui/icons/SubdirectoryArrowLeft';
+import * as setISODay from 'date-fns/set_iso_day';
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { Lang } from '../contexts/types';
+import { useFormatDate, useTranslations } from '../utils/hooks';
+import Tooltip from './Tooltip';
+
+const InputGroup = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5em;
+`;
+
+interface Props {
+  lang?: Lang;
+  defaultValue: Array<number[] | null>;
+  disabled?: boolean;
+  onChange(hours: Array<number[] | null>): void;
+}
+
+const OpeningHoursInput = (props: Props) => {
+  const translations = useTranslations();
+  const formatDate = useFormatDate();
+  const [openingHours, setOpeningHours] = React.useState<
+    Array<string[] | null>
+  >([]);
+  React.useEffect(() => {
+    setOpeningHours(
+      props.defaultValue.map(hours => {
+        if (!hours) {
+          return null;
+        }
+        return hours.map((hour: number) =>
+          String(hour).length === 4
+            ? String(hour).substr(0, 2) + ':' + String(hour).substr(2)
+            : String(hour)
+        );
+      })
+    );
+  }, [props.defaultValue]);
+
+  React.useEffect(() => {
+    props.onChange(
+      openingHours.map(hours =>
+        hours ? hours.map(h => Number(h.replace(':', ''))) : null
+      )
+    );
+  }, [openingHours]);
+
+  return (
+    <React.Fragment>
+      {openingHours.map((times, i) => {
+        const isClosed = times === null;
+        const weekDayLabel = formatDate(setISODay(new Date(), i + 1), 'dddd');
+
+        const createDayToggler = (index: number) => (
+          event: React.ChangeEvent<HTMLInputElement>
+        ) => {
+          const hours = [...openingHours];
+          if (event.target.checked) {
+            hours[index] = ['', ''];
+          } else {
+            hours[index] = null;
+          }
+          setOpeningHours(hours);
+        };
+
+        const createDayTimeChanger = (dayIndex: number, timeIndex: number) => (
+          event: React.ChangeEvent<HTMLInputElement>
+        ) => {
+          const hours = [...openingHours];
+          hours[dayIndex][timeIndex] = event.target.value;
+          setOpeningHours(hours);
+        };
+
+        const createCopyFromPrevious = (dayIndex: number) => () => {
+          const hours = [...openingHours];
+          hours[dayIndex] = hours[dayIndex - 1];
+          setOpeningHours(hours);
+        };
+
+        return (
+          <InputGroup key={i}>
+            <Checkbox
+              disabled={props.disabled}
+              onChange={createDayToggler(i)}
+              checked={!isClosed}
+            />
+            <TextField
+              label={`${weekDayLabel}, ${translations.openingTime}`}
+              style={{ margin: 4 }}
+              fullWidth
+              margin="dense"
+              inputProps={{
+                pattern: '[0-9]{1,}:[0-9]{2}'
+              }}
+              disabled={isClosed || props.disabled}
+              InputLabelProps={{ shrink: true }}
+              value={isClosed ? translations.closed : times[0]}
+              onChange={createDayTimeChanger(i, 0)}
+            />
+            <TextField
+              label={`${weekDayLabel}, ${translations.closingTime}`}
+              style={{ margin: 4 }}
+              fullWidth
+              margin="dense"
+              inputProps={{
+                pattern: '[0-9]{1,}:[0-9]{2}'
+              }}
+              disabled={isClosed || props.disabled}
+              InputLabelProps={{ shrink: true }}
+              value={isClosed ? translations.closed : times[1]}
+              onChange={createDayTimeChanger(i, 1)}
+            />
+            <Tooltip translationKey="copyFromPreviousDay">
+              <IconButton
+                onClick={createCopyFromPrevious(i)}
+                disabled={i === 0 || isClosed}
+                aria-label="Copy from previous"
+              >
+                <CopyFromPrevious />
+              </IconButton>
+            </Tooltip>
+          </InputGroup>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default OpeningHoursInput;

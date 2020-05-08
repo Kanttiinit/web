331d99d1410c3e9b0a695bc8d7174c522c9e1d34
Max Huttunen <max.huttunen@gmail.com>
2020-05-08 12:33:03 +0300

    read dotenv in server js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const semver = require('semver')
-const pkg = require('./package.json')
+require('dotenv').config();
+const express = require('express');
+const semver = require('semver');
+const pkg = require('./package.json');
 
 express()
-.use((req, res, next) => {
-  if (req.headers.host.slice(0, 4) === 'www.') {
-    res.redirect(301, `${req.protocol}://${req.headers.host.slice(4)}${req.originalUrl}`)
-  } else {
-    next()
-  }
-})
-.use(require('compression')())
-.use(express.static('dist'))
-.use(express.static(__dirname + '/src/assets'))
-.get('/robots.txt', (req, res) => {
-  res.send('');
-})
-.get('/admin*', (req, res) => res.sendFile(__dirname + '/dist/index_admin.html'))
-.get('/check-update', (req, res) => {
-  try {
-    res.json({updateAvailable: semver.gt(pkg.version, req.query.version)})
-  } catch (e) {
-    res.status(400).json({error: true})
-  }
-})
-.get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
-.listen(process.env.PORT || 8080)
+  .use((req, res, next) => {
+    if (req.headers.host.slice(0, 4) === 'www.') {
+      res.redirect(
+        301,
+        `${req.protocol}://${req.headers.host.slice(4)}${req.originalUrl}`
+      );
+    } else {
+      next();
+    }
+  })
+  .use(require('compression')())
+  .use(express.static('dist'))
+  .use(express.static(__dirname + '/src/assets'))
+  .get('/robots.txt', (req, res) => {
+    res.send('');
+  })
+  .get('/admin*', (req, res) =>
+    res.sendFile(__dirname + '/dist/index_admin.html')
+  )
+  .get('/check-update', (req, res) => {
+    try {
+      res.json({ updateAvailable: semver.gt(pkg.version, req.query.version) });
+    } catch (e) {
+      res.status(400).json({ error: true });
+    }
+  })
+  .get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
+  .listen(process.env.PORT || 8080);

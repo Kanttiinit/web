f7b6a50acb5d16c108cadbf38841beb65a99dd47
Max Huttunen <max.huttunen@gmail.com>
2020-05-11 21:05:00 +0300

    simplify server index

--- index.js
+++ index.js
@@ -4,22 +4,8 @@ const semver = require('semver');
 const pkg = require('./package.json');
 
 express()
-  .use((req, res, next) => {
-    if (req.headers.host.slice(0, 4) === 'www.') {
-      res.redirect(
-        301,
-        `${req.protocol}://${req.headers.host.slice(4)}${req.originalUrl}`
-      );
-    } else {
-      next();
-    }
-  })
-  .use(require('compression')())
   .use(express.static('dist'))
   .use(express.static(__dirname + '/src/assets'))
-  .get('/robots.txt', (req, res) => {
-    res.send('');
-  })
   .get('/admin*', (req, res) =>
     res.sendFile(__dirname + '/dist/index_admin.html')
   )

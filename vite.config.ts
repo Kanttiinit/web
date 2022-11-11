4ae55253c1b81b3d3b74de35933c98acee6955ea
max <max.huttunen@gmail.com>
2022-11-11 15:05:46 +0200

    make PWA-aware

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'prompt',
+      manifest: {
+        name: 'Kanttiinit',
+        short_name: 'Kanttiinit',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#f4f4f4',
+        theme_color: '#009688',
+        description: 'Student lunch effortlessly.',
+        lang: 'en-US',
+        orientation: 'any',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '800x800',
+            type: 'image/png'
+          }
+        ],
+        related_applications: [
+          {
+            platform: 'web',
+            url: 'https://kanttiinit.fi/'
+          },
+          {
+            platform: 'itunes',
+            url: 'https://itunes.apple.com/fi/app/kanttiinit/id1069903670?l=fi&mt=8'
+          }
+        ]
+      }
+    })
+  ],
   define: {
     'VERSION': JSON.stringify(process.env.npm_package_version),
     'PUBLIC_ASSET_PATH': JSON.stringify('/'),

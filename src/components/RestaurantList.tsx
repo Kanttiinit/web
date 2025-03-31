afc7cd14a1d95364818347f3b530e7d09ba32075
Jere Vaara <jerevaara@gmail.com>
2025-03-31 17:04:09 +0300

    fix lint

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -6,10 +6,92 @@ import NetworkStatus from './NetworkStatus';
 import Notice from './Notice';
 import Restaurant, { Placeholder } from './Restaurant';
 import { breakLarge, breakSmall } from '../globalStyles';
-import { For, Match, Switch } from 'solid-js';
+import { For, Match, Switch, createSignal } from 'solid-js';
 import { computedState, state, resources } from '../state';
 import { WarningIcon } from '../icons';
 import { useFormattedRestaurants } from '../utils';
+
+const MakeItStopButton = styled.button`
+  background-color: tomato;
+  border-radius: 1rem;
+  border: none;
+  color: white;
+  font-size: 20pt;
+  padding: 0.5rem 1rem;
+  max-width: 180px;
+  position: fixed;
+  top: 1rem;
+  margin: auto;
+  right: 1rem;
+  left: 1rem;
+  z-index: 1000;
+
+  &:hover,
+  &:focus {
+    top: 0.5rem;
+    transition: top 0.2s ease;
+  }
+
+  cursor: pointer;
+  animation: fadeInDown 0.6s ease-out;
+
+  @keyframes fadeInDown {
+    from {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+const ContribModal = styled.div`
+  background-color: var(--gray6);
+  border-radius: 0.8rem;
+  border: 1px var(--gray6) solid;
+  box-shadow: 0rem 0.1rem 0.6rem -0.2rem rgba(0, 0, 0, 0.3);
+  color: var(--gray1);
+  font-size: 16pt;
+  padding: 0.5rem 1rem;
+  max-width: 400px;
+  position: fixed;
+  top: 1rem;
+  margin: auto;
+  right: 1rem;
+  left: 1rem;
+  z-index: 1000;
+  animation: fadeInDown 0.6s ease-out both;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 10px;
+
+  button {
+    border: none;
+    padding: 0.8em 1.2em;
+    border-radius: 0.4em;
+    font-family: inherit;
+    font-size: 0.8rem;
+    display: inline-block;
+    min-width: 4rem;
+    background: var(--gray3);
+    text-align: center;
+    color: var(--gray6);
+    outline: none;
+    font-weight: 500;
+    transition: transform 0.1s;
+  }
+
+  a {
+    color: slateblue;
+    text-decoration: underline;
+    font-weight: 500;
+    transition: transform 0.1s;
+    margin-top: 1rem;
+  }
+`;
 
 const Container = styled.main`
   padding: 4rem 0 1.5rem;
@@ -53,6 +135,18 @@ const placeholders = Array(8)
   .map(() => <Placeholder />);
 
 function ListContent() {
+  const [showMakeItStaph, setShowMakeItStaph] = createSignal(false);
+  const [showContribLink, setShowContribLink] = createSignal(false);
+
+  function onShowMakeItStaph() {
+    setShowMakeItStaph(true);
+  }
+  function makeItStaph() {
+    localStorage.setItem('isSurprise', 'false');
+    setShowContribLink(true);
+    setShowMakeItStaph(false);
+  }
+
   const loading = () =>
     resources.menus[0].loading ||
     resources.restaurants[0].loading ||
@@ -88,8 +182,34 @@ function ListContent() {
         </Notice>
       </Match>
       <Match when={true}>
+        {showMakeItStaph() && (
+          <MakeItStopButton onClick={makeItStaph}>
+            make it stop
+          </MakeItStopButton>
+        )}
+        {showContribLink() && (
+          <ContribModal>
+            <p>
+              Thanks for using Kanttiinit.fi! Visit our GitHub page to
+              contribute to the project and help us keep it alive.
+            </p>
+            <a
+              href="https://kanttiinit.github.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              kanttiinit.github.io
+            </a>
+            <button onClick={() => setShowContribLink(false)}>close</button>
+          </ContribModal>
+        )}
         <For each={useFormattedRestaurants()}>
-          {restaurant => <Restaurant restaurant={restaurant} />}
+          {restaurant => (
+            <Restaurant
+              restaurant={restaurant}
+              onShowMakeItStaph={onShowMakeItStaph}
+            />
+          )}
         </For>
       </Match>
     </Switch>
